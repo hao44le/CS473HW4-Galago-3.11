720a61425f71663e0bbc3ffcb42f1df4463f5e97
Marc Cartright <irmarc@cs.umass.edu>
2012-08-01 18:41:04 -0400

    Updated the pom files to remove an error due to using an incorrect substitution.

---
+++ core/src/main/java/org/lemurproject/galago/core/parse/MBTEIPersonParser.java
@@ -0,0 +1,13 @@
+// BSD License (http://lemurproject.org/galago-license)
+package org.lemurproject.galago.core.parse;
+
+import org.lemurproject.galago.core.types.DocumentSplit;
+import org.lemurproject.galago.tupleflow.Parameters;
+
+class MBTEIPersonParser extends MBTEIEntityParser {
+
+  public MBTEIPersonParser(DocumentSplit split, Parameters p) {
+    super(split, p);
+    restrict = "per";
+  }
+}
