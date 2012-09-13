4089c86fed33237af08a5d37076142d238eac9f1
sjh <sjh@csreg015.csdhcp.cs.local>
2012-09-13 11:29:30 -0400

    Minor changes: made a field protected in pos-idx-reader, added checks for empty combine operators - now throws exceptions

---
+++ core/src/main/java/org/lemurproject/galago/core/parse/UniversalParser.java
@@ -0,0 +1,155 @@
+// BSD License (http://lemurproject.org/galago-license)
+package org.lemurproject.galago.core.parse;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.List;
+import java.util.logging.Logger;
+import org.lemurproject.galago.tupleflow.Counter;
+import org.lemurproject.galago.tupleflow.InputClass;
+import org.lemurproject.galago.tupleflow.OutputClass;
+import org.lemurproject.galago.tupleflow.StandardStep;
+import org.lemurproject.galago.tupleflow.execution.Verified;
+import org.lemurproject.galago.tupleflow.TupleFlowParameters;
+import org.lemurproject.galago.core.types.DocumentSplit;
+import org.lemurproject.galago.tupleflow.*;
+
+/**
+ * Determines the class type of the input split, either based on the "filetype"
+ * parameter passed in, or by guessing based on the file path extension.
+ *
+ * (7/29/2012, irmarc): Refactored to be plug-and-play. External filetypes may
+ * be added via the parameters.
+ *
+ * Instantiation of a type-specific parser (TSP) is done by the UniversalParser.
+ * It checks the formal argument types of the (TSP) to match on the possible
+ * input methods it has available (i.e. an inputstream or a buffered reader over
+ * the input data. Additionally, any TSP may have TupleFlowParameters in its
+ * formal argument list, and the parameters provided to the UniversalParser will
+ * be forwarded to the TSP instance.
+ *
+ * @author trevor, sjh, irmarc
+ */
+@Verified
+@InputClass(className = "org.lemurproject.galago.core.types.DocumentSplit")
+@OutputClass(className = "org.lemurproject.galago.core.parse.Document")
+public class UniversalParser extends StandardStep<DocumentSplit, Document> {
+
+  // The built-in type map
+  static String[][] sFileTypeLookup = {
+    {"html", FileParser.class.getName()},
+    {"xml", FileParser.class.getName()},
+    {"txt", FileParser.class.getName()},
+    {"arc", ArcParser.class.getName()},
+    {"warc", WARCParser.class.getName()},
+    {"trectext", TrecTextParser.class.getName()},
+    {"trecweb", TrecWebParser.class.getName()},
+    {"twitter", TwitterParser.class.getName()},
+    {"corpus", CorpusSplitParser.class.getName()},
+    {"wiki", WikiParser.class.getName()},
+    {"mbtei.page", MBTEIPageParser.class.getName()},
+    {"mbtei.book", MBTEIBookParser.class.getName()},
+    {"mbtei.entity", MBTEIEntityParser.class.getName()},
+    {"mbtei.person", MBTEIPersonParser.class.getName()},
+    {"mbtei.location", MBTEILocationParser.class.getName()}
+  };
+  private Counter documentCounter;
+  private TupleFlowParameters tfParameters;
+  private Parameters parameters;
+  private Logger logger = Logger.getLogger(getClass().toString());
+  private byte[] subCollCheck = "subcoll".getBytes();
+  private HashMap<String, Class> documentStreamParsers;
+  private HashMap<String, Parameters> documentStreamParserParameters;
+
+  public UniversalParser(TupleFlowParameters parameters) {
+    this.documentCounter = parameters.getCounter("Documents Parsed");
+    this.tfParameters = parameters;
+    this.parameters = parameters.getJSON();
+    buildFileTypeMap();
+  }
+
+  private void buildFileTypeMap() {
+    try {
+      documentStreamParsers = new HashMap<String, Class>();
+      documentStreamParserParameters = new HashMap<String, Parameters>();
+      for (String[] mapping : sFileTypeLookup) {
+        documentStreamParsers.put(mapping[0], Class.forName(mapping[1]));
+        documentStreamParserParameters.put(mapping[0], new Parameters());
+      }
+
+      // Look for external mapping definitions
+      if (parameters.isList("externalParsers", Parameters.Type.MAP)
+              || parameters.isMap("externalParsers")) {
+        List<Parameters> externalParsers =
+                (List<Parameters>) parameters.getAsList("externalParsers");
+
+        for (Parameters extP : externalParsers) {
+          if (extP.isString("filetype") && extP.isString("class")) {
+            documentStreamParsers.put(extP.getString("filetype"),
+                    Class.forName(extP.getString("class")));
+            documentStreamParserParameters.put(extP.getString("filetype"), extP);
+          } else {
+            logger.log(Level.SEVERE, "Could not parse manually defined filetype :{0}\n ''filetype'' and ''class'' are required keys.", extP.toString());
+            throw new IllegalArgumentException("Could not parse manually defined filetype : " + extP.toString() + "\n ''filetype'' and ''class'' are required fields.");
+          }
+        }
+      }
+
+    } catch (Exception ex) {
+      throw new IllegalArgumentException(ex);
+    }
+  }
+
+  public boolean isParsable(String extension) {
+    return this.documentStreamParsers.containsKey(extension);
+  }
+
+  @Override
+  public void process(DocumentSplit split) throws IOException {
+    long count = 0;
+    long limit = Long.MAX_VALUE;
+    if (split.startKey.length > 0) {
+      if (Utility.compare(subCollCheck, split.startKey) == 0) {
+        limit = Utility.uncompressLong(split.endKey, 0);
+      }
+    }
+
+    if (this.documentStreamParsers.containsKey(split.fileType)) {
+      try {
+        Class c = documentStreamParsers.get(split.fileType);
+        Constructor cstr = c.getConstructor(DocumentSplit.class, Parameters.class);        
+        DocumentStreamParser parser = (DocumentStreamParser) cstr.newInstance(split, documentStreamParserParameters.get(split.fileType));
+
+        Document document;
+        while ((document = parser.nextDocument()) != null) {
+          document.fileId = split.fileId;
+          document.totalFileCount = split.totalFileCount;
+
+          processor.process(document);
+          if (documentCounter != null) {
+            documentCounter.increment();
+          }
+          count++;
+
+          // Enforces limitations imposed by the endKey subcollection specifier.
+          // See DocumentSource for details.
+          if (count >= limit) {
+            break;
+          }
+        }
+
+        if (parser != null) {
+          parser.close();
+        }
+
+      } catch (Exception ex) {
+        logger.log(Level.INFO, "Failed to parse document split - {0} as {1}\n", new Object[]{split.toString(), split.fileType});
+        logger.log(Level.SEVERE, ex.toString());
+      }
+    } else {
+      logger.log(Level.INFO, "Ignoring {0} - could not find a parser for file-type:{1}\n", new Object[]{split.toString(), split.fileType});
+    }
+  }
+}
---
+++ core/src/test/java/org/lemurproject/galago/core/index/disk/DiskLengthsTest.java
@@ -0,0 +1,127 @@
+/*
+ *  BSD License (http://lemurproject.org/galago-license)
+ */
+package org.lemurproject.galago.core.index.disk;
+
+import java.io.File;
+import java.io.IOException;
+import junit.framework.TestCase;
+import org.lemurproject.galago.core.index.disk.DiskLengthsReader.KeyIterator;
+import org.lemurproject.galago.core.index.disk.DiskLengthsReader.MemoryMapLengthsIterator;
+import org.lemurproject.galago.core.index.disk.DiskLengthsReader.StreamLengthsIterator;
+import org.lemurproject.galago.core.retrieval.processing.ScoringContext;
+import org.lemurproject.galago.core.types.FieldLengthData;
+import org.lemurproject.galago.tupleflow.FakeParameters;
+import org.lemurproject.galago.tupleflow.Parameters;
+import org.lemurproject.galago.tupleflow.Utility;
+
+/**
+ *
+ * @author sjh
+ */
+public class DiskLengthsTest extends TestCase {
+
+  public DiskLengthsTest(String name) {
+    super(name);
+  }
+
+  public void testLengths() throws IOException {
+    File len = null;
+    try {
+
+      len = Utility.createTemporary();
+
+      Parameters p = new Parameters();
+      p.set("filename", len.getAbsolutePath());
+      DiskLengthsWriter writer = new DiskLengthsWriter(new FakeParameters(p));
+
+      byte[] key = Utility.fromString("document");
+      for (int i = 10; i <= 100; i++) {
+        writer.process(new FieldLengthData(key, i, i + 1));
+      }
+
+      writer.process(new FieldLengthData(key, 110, 111));
+
+      writer.close();
+
+      DiskLengthsReader reader = new DiskLengthsReader(len.getAbsolutePath());
+
+      // first some random seeks
+      assertEquals(reader.getLength(90), 91);
+      assertEquals(reader.getLength(50), 51);
+      assertEquals(reader.getLength(105), 0);
+      assertEquals(reader.getLength(110), 111);
+
+      KeyIterator ki = reader.getIterator();
+      MemoryMapLengthsIterator memItr = ki.getMemoryValueIterator();
+      StreamLengthsIterator streamItr = ki.getStreamValueIterator();
+      
+      ScoringContext sc = new ScoringContext();
+      memItr.setContext(sc);
+      streamItr.setContext(sc);
+      
+      while (!memItr.isDone() || !streamItr.isDone()) {
+
+        assertEquals(memItr.getCurrentIdentifier(), streamItr.getCurrentIdentifier());
+        sc.document = memItr.getCurrentIdentifier();
+        
+        assertEquals(memItr.getCurrentLength(), streamItr.getCurrentLength());
+
+        memItr.movePast(memItr.currentCandidate());
+        streamItr.movePast(streamItr.currentCandidate());
+      }
+
+      memItr.reset();
+      streamItr.reset();
+
+      memItr.syncTo(50);
+      streamItr.syncTo(50);
+      sc.document = 50;
+      assertEquals(memItr.currentCandidate(), 50);
+      assertEquals(streamItr.currentCandidate(), 50);
+      assertEquals(memItr.getCurrentLength(), 51);
+      assertEquals(streamItr.getCurrentLength(), 51);
+
+      memItr.syncTo(90);
+      streamItr.syncTo(90);
+      sc.document = 90;
+      assertEquals(memItr.currentCandidate(), 90);
+      assertEquals(streamItr.currentCandidate(), 90);
+      assertEquals(memItr.getCurrentLength(), 91);
+      assertEquals(streamItr.getCurrentLength(), 91);
+
+      memItr.syncTo(90);
+      streamItr.syncTo(90);
+      sc.document = 90;
+      assertEquals(memItr.currentCandidate(), 90);
+      assertEquals(streamItr.currentCandidate(), 90);
+      assertEquals(memItr.getCurrentLength(), 91);
+      assertEquals(streamItr.getCurrentLength(), 91);
+
+      memItr.syncTo(110);
+      streamItr.syncTo(110);
+      sc.document = 110;
+      assertEquals(memItr.currentCandidate(), 110);
+      assertEquals(streamItr.currentCandidate(), 110);
+      assertEquals(memItr.getCurrentLength(), 111);
+      assertEquals(streamItr.getCurrentLength(), 111);
+
+      memItr.syncTo(200);
+      streamItr.syncTo(200);
+      sc.document = 200;
+      assertEquals(memItr.currentCandidate(), 110);
+      assertEquals(streamItr.currentCandidate(), 110);
+      assertEquals(memItr.getCurrentLength(), 0);
+      assertEquals(streamItr.getCurrentLength(), 0);
+
+      reader.close();
+
+
+    } finally {
+      if (len != null) {
+        len.delete();
+      }
+
+    }
+  }
+}
