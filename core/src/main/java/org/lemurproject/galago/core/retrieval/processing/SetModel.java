a6f4b40fd7f0fd5b14d38ea03639126807dfc8ee
sjh <sjh@localhost>
2013-01-25 11:45:06 -0500

    bug fix for dump-term-stats

---
+++ core/src/main/java/org/lemurproject/galago/core/retrieval/processing/SetModel.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.lemurproject.galago.core.retrieval.processing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.lemurproject.galago.core.index.Index;
+import org.lemurproject.galago.core.retrieval.LocalRetrieval;
+import org.lemurproject.galago.core.retrieval.ScoredDocument;
+import org.lemurproject.galago.core.retrieval.iterator.MovableIndicatorIterator;
+import org.lemurproject.galago.core.retrieval.query.Node;
+import org.lemurproject.galago.tupleflow.Parameters;
+
+/**
+ * Implements standard boolean processing model. Results are not ranked, just
+ * returned.
+ *
+ *
+ * @author irmarc
+ */
+public class SetModel extends ProcessingModel {
+
+  LocalRetrieval retrieval;
+  Index index;
+
+  public SetModel(LocalRetrieval lr) {
+    retrieval = lr;
+    this.index = retrieval.getIndex();
+  }
+
+  @Override
+  public ScoredDocument[] execute(Node queryTree, Parameters queryParams) throws Exception {
+    ScoringContext context = new ScoringContext();
+
+    // construct the query iterators
+    MovableIndicatorIterator iterator = 
+            (MovableIndicatorIterator) retrieval.createIterator(queryParams, 
+                        queryTree, 
+                        context);
+    ArrayList<ScoredDocument> list = new ArrayList<ScoredDocument>();
+    while (!iterator.isDone()) {
+
+      // ensure we are at the document we wish to score
+      // -- this function will move ALL iterators, 
+      //     not just the ones that do not have all candidates
+      iterator.syncTo(iterator.currentCandidate());
+
+      if (iterator.hasMatch(iterator.currentCandidate())) {
+        list.add(new ScoredDocument(iterator.currentCandidate(), 1.0));
+      }
+      iterator.movePast(iterator.currentCandidate());
+    }
+    return list.toArray(new ScoredDocument[0]);
+  }
+}
---
+++ core/src/test/java/org/lemurproject/galago/core/scoring/DirichletScorerTest.java
@@ -0,0 +1,46 @@
+// BSD License (http://lemurproject.org/galago-license)
+package org.lemurproject.galago.core.scoring;
+
+import org.lemurproject.galago.core.scoring.DirichletScorer;
+import java.io.IOException;
+import junit.framework.TestCase;
+import org.lemurproject.galago.core.retrieval.query.NodeParameters;
+import org.lemurproject.galago.tupleflow.Parameters;
+
+/**
+ *
+ * @author trevor
+ */
+public class DirichletScorerTest extends TestCase {
+
+  public DirichletScorerTest(String testName) {
+    super(testName);
+  }
+
+  public void testCollectionProbability() throws IOException {
+    NodeParameters p = new NodeParameters();
+    p.set("nodeFrequency", 1);
+    p.set("collectionLength", 2);
+    DirichletScorer scorer = new DirichletScorer(p, null);
+
+    assertEquals(1500.0, scorer.mu);
+    assertEquals(0.5, scorer.background);
+
+    double score = scorer.score(15, 100);
+    assertEquals(-0.73788, score, 0.001);
+  }
+
+  public void testSetMu() throws IOException {
+    NodeParameters p = new NodeParameters();
+    p.set("nodeFrequency", 1);
+    p.set("collectionLength", 2);
+    p.set("mu", 13);
+    DirichletScorer scorer = new DirichletScorer(p, null);
+
+    assertEquals(13.0, scorer.mu);
+    assertEquals(0.5, scorer.background);
+
+    double score = scorer.score(5, 100);
+    assertEquals(-2.28504, score, 0.001);
+  }
+}
