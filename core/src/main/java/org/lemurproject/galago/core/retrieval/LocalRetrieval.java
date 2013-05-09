38128438b3ff10854e5fadc3195e7ad0f72739a5
sjh <sjh@localhost>
2013-05-09 10:01:42 -0400

    bug fix for threaded executor (stops hanging at completion

---
+++ core/src/main/java/org/lemurproject/galago/core/retrieval/LocalRetrieval.java
@@ -0,0 +1,462 @@
+// BSD License (http://lemurproject.org/galago-license)
+package org.lemurproject.galago.core.retrieval;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import org.lemurproject.galago.core.index.AggregateReader.CollectionAggregateIterator;
+import org.lemurproject.galago.core.index.AggregateReader.CollectionStatistics;
+import org.lemurproject.galago.core.index.AggregateReader.IndexPartStatistics;
+import org.lemurproject.galago.core.index.AggregateReader.NodeAggregateIterator;
+import org.lemurproject.galago.core.index.AggregateReader.NodeStatistics;
+import org.lemurproject.galago.core.index.Index;
+import org.lemurproject.galago.core.index.NamesReader.NamesIterator;
+import org.lemurproject.galago.core.index.disk.DiskIndex;
+import org.lemurproject.galago.core.parse.Document;
+import org.lemurproject.galago.core.retrieval.iterator.ContextualIterator;
+import org.lemurproject.galago.core.retrieval.iterator.CountIterator;
+import org.lemurproject.galago.core.retrieval.iterator.IndicatorIterator;
+import org.lemurproject.galago.core.retrieval.iterator.MovableCountIterator;
+import org.lemurproject.galago.core.retrieval.iterator.MovableIterator;
+import org.lemurproject.galago.core.retrieval.iterator.MovableLengthsIterator;
+import org.lemurproject.galago.core.retrieval.iterator.ScoreIterator;
+import org.lemurproject.galago.core.retrieval.iterator.ScoringFunctionIterator;
+import org.lemurproject.galago.core.retrieval.processing.ActiveContext;
+import org.lemurproject.galago.core.retrieval.processing.ProcessingModel;
+import org.lemurproject.galago.core.retrieval.processing.ScoringContext;
+import org.lemurproject.galago.core.retrieval.query.Node;
+import org.lemurproject.galago.core.retrieval.query.NodeType;
+import org.lemurproject.galago.core.retrieval.query.QueryType;
+import org.lemurproject.galago.core.retrieval.query.StructuredQuery;
+import org.lemurproject.galago.core.retrieval.structured.ContextFactory;
+import org.lemurproject.galago.core.retrieval.structured.FeatureFactory;
+import org.lemurproject.galago.core.retrieval.traversal.Traversal;
+import org.lemurproject.galago.tupleflow.Parameters;
+import org.lemurproject.galago.tupleflow.Utility;
+
+/**
+ * The responsibility of the LocalRetrieval object is to provide a simpler
+ * interface on top of the DiskIndex. Therefore, given a query or text string
+ * representing a query, this object will perform the necessary transformations
+ * to make it an executable object.
+ *
+ * 10/7/2010 - Modified for asynchronous execution
+ *
+ * @author trevor
+ * @author irmarc
+ */
+public class LocalRetrieval implements Retrieval {
+
+  protected final Logger logger = Logger.getLogger(this.getClass().getName());
+  protected Index index;
+  protected FeatureFactory features;
+  protected Parameters globalParameters;
+  protected CachedRetrieval cache;
+
+  /**
+   * One retrieval interacts with one index. Parameters dictate the behavior
+   * during retrieval time, and selection of the appropriate feature factory.
+   * Additionally, the supplied parameters will be passed forward to the chosen
+   * feature factory.
+   */
+  public LocalRetrieval(Index index) throws Exception {
+    this(index, new Parameters());
+  }
+
+  public LocalRetrieval(String filename, Parameters parameters) throws Exception {
+    this(new DiskIndex(filename), parameters);
+  }
+
+  public LocalRetrieval(Index index, Parameters parameters) throws Exception {
+    this.globalParameters = parameters;
+    setIndex(index);
+  }
+
+  protected void setIndex(Index indx) throws Exception {
+    this.index = indx;
+    features = new FeatureFactory(globalParameters);
+    cache = null;
+    if (this.globalParameters.get("cache", false)) {
+      cache = new CachedRetrieval(this.globalParameters);
+    }
+  }
+
+  /**
+   * Closes the underlying index
+   */
+  @Override
+  public void close() throws IOException {
+    index.close();
+  }
+
+  /**
+   * Returns the collectionLength and documentCount of a given index, contained
+   * in a Parameters object. Additional statistics may be provided, but are not
+   * expected.
+   */
+  @Override
+  public IndexPartStatistics getIndexPartStatistics(String partName) throws IOException {
+    return index.getIndexPartStatistics(partName);
+  }
+
+  @Override
+  public Parameters getGlobalParameters() {
+    return this.globalParameters;
+  }
+
+  /*
+   * {
+   * <partName> : { <nodeName> : <iteratorClass>, stemming : false, ... },
+   * <partName> : { <nodeName> : <iteratorClass>, ... }, ... }
+   */
+  @Override
+  public Parameters getAvailableParts() throws IOException {
+    Parameters p = new Parameters();
+    for (String partName : index.getPartNames()) {
+      Parameters inner = new Parameters();
+      Map<String, NodeType> nodeTypes = index.getPartNodeTypes(partName);
+      for (String nodeName : nodeTypes.keySet()) {
+        inner.set(nodeName, nodeTypes.get(nodeName).getIteratorClass().getName());
+      }
+      p.set(partName, inner);
+    }
+    return p;
+  }
+
+  public Index getIndex() {
+    return index;
+  }
+
+  @Override
+  public Document getDocument(String identifier, Parameters p) throws IOException {
+    return this.index.getDocument(identifier, p);
+  }
+
+  @Override
+  public Map<String, Document> getDocuments(List<String> identifier, Parameters p) throws IOException {
+    return this.index.getDocuments(identifier, p);
+  }
+
+  /**
+   * Accepts a transformed query, constructs the iterator tree from the node
+   * tree, then iterates over the iterator tree, and returns the results.
+   */
+  public ScoredDocument[] runQuery(String query, Parameters p) throws Exception {
+    Node root = StructuredQuery.parse(query);
+    root = transformQuery(root, p);
+    return runQuery(root, p);
+  }
+
+  @Override
+  public ScoredDocument[] runQuery(Node queryTree) throws Exception {
+    return runQuery(queryTree, new Parameters());
+  }
+
+  // Based on the root of the tree, that dictates how we execute.
+  @Override
+  public ScoredDocument[] runQuery(Node queryTree, Parameters queryParams) throws Exception {
+    ScoredDocument[] results = null;
+    if (globalParameters.containsKey("processingModel")) {
+      queryParams.set("processingModel", globalParameters.getString("processingModel"));
+    }
+    ProcessingModel pm = ProcessingModel.instance(this, queryTree, queryParams);
+
+    // get some results
+    results = pm.execute(queryTree, queryParams);
+    if (results == null) {
+      results = new ScoredDocument[0];
+    }
+
+    // Format and get names
+    String indexId = this.globalParameters.get("indexId", "0");
+    return getArrayResults(results, indexId);
+  }
+
+  /*
+   * getArrayResults annotates a queue of scored documents returns an array
+   *
+   */
+  protected <T extends ScoredDocument> T[] getArrayResults(T[] results, String indexId) throws IOException {
+    if (results == null || results.length == 0) {
+      return null;
+    }
+
+    for (int i = 0; i < results.length; i++) {
+      results[i].source = indexId;
+      results[i].rank = i + 1;
+    }
+
+    // this is to assign proper document names
+    T[] byID = Arrays.copyOf(results, results.length);
+
+    Arrays.sort(byID, new Comparator<T>() {
+
+      @Override
+      public int compare(T o1, T o2) {
+        return Utility.compare(o1.document, o2.document);
+      }
+    });
+
+    NamesIterator namesIterator = index.getNamesIterator();
+    ScoringContext sc = new ScoringContext();
+    namesIterator.setContext(sc);
+
+    for (T doc : byID) {
+      namesIterator.syncTo(doc.document);
+      sc.document = doc.document;
+
+      if (doc.document == namesIterator.getCurrentIdentifier()) {
+        doc.documentName = namesIterator.getCurrentName();
+      } else {
+        System.err.println("NAMES ITERATOR FAILED TO FIND DOCUMENT " + doc.document);
+        // now throw an error.
+        doc.documentName = index.getName(doc.document);
+      }
+    }
+
+    return results;
+  }
+
+  public MovableIterator createIterator(Parameters queryParameters, Node node, ScoringContext context) throws Exception {
+    if (globalParameters.get("shareNodes", true)) {
+      if (queryParameters.get("shareNodes", true)) {
+        return createNodeMergedIterator(node, context, new HashMap());
+      }
+    }
+    return createNodeMergedIterator(node, context, null);
+  }
+
+  protected MovableIterator createNodeMergedIterator(Node node, ScoringContext context,
+          HashMap<String, MovableIterator> queryIteratorCache)
+          throws Exception {
+    ArrayList<MovableIterator> internalIterators = new ArrayList<MovableIterator>();
+    MovableIterator iterator;
+
+    // first check if this is a repeated node in this tree:
+    if (queryIteratorCache != null && queryIteratorCache.containsKey(node.toString())) {
+      iterator = queryIteratorCache.get(node.toString());
+      context.toNodes.put(iterator, node);
+      return iterator;
+    }
+
+    // second check if this node is cached
+    if (cache != null && cache.isCached(node)) {
+      iterator = cache.getCachedIterator(node);
+    } else {
+
+      // otherwise we need to create a new iterator
+      // start by recursively creating children
+      for (Node internalNode : node.getInternalNodes()) {
+        MovableIterator internalIterator = createNodeMergedIterator(internalNode, context, queryIteratorCache);
+        internalIterators.add(internalIterator);
+      }
+
+      iterator = index.getIterator(node);
+      if (iterator == null) {
+        iterator = features.getIterator(node, internalIterators);
+      }
+    }
+
+    // we have now constructed the iterator from the cache or from the index:
+    //  --> deal with the context
+    if (context != null && ContextualIterator.class.isAssignableFrom(iterator.getClass())) {
+      ((ContextualIterator) iterator).setContext(context);
+    }
+
+    if (context != null && ActiveContext.class.isAssignableFrom(context.getClass())) {
+      ((ActiveContext) context).checkIterator(node, iterator);
+    }
+
+    // we've created a new iterator - add to the cache for future nodes
+    if (queryIteratorCache != null) {
+      queryIteratorCache.put(node.toString(), iterator);
+    }
+
+    context.toNodes.put(iterator, node);
+    return iterator;
+  }
+
+  @Override
+  public Node transformQuery(Node queryTree, Parameters queryParams) throws Exception {
+    return transformQuery(features.getTraversals(this, queryTree, queryParams), queryTree);
+  }
+
+  protected Node transformQuery(List<Traversal> traversals, Node queryTree) throws Exception {
+    for (Traversal traversal : traversals) {
+      queryTree = StructuredQuery.walk(traversal, queryTree);
+      // System.out.println(traversal.getClass().getSimpleName() + "\t" + queryTree.toPrettyString());
+    }
+    return queryTree;
+  }
+
+  @Override
+  public CollectionStatistics getCollectionStatistics(String nodeString) throws Exception {
+    // first parse the node
+    Node root = StructuredQuery.parse(nodeString);
+    return getCollectionStatistics(root);
+  }
+
+  @Override
+  public CollectionStatistics getCollectionStatistics(Node root) throws Exception {
+
+    ScoringContext sc = ContextFactory.createContext(globalParameters);
+    MovableIterator structIterator = createIterator(new Parameters(), root, sc);
+
+    // first check if this iterator is an aggregate iterator (has direct access to stats)
+    if (CollectionAggregateIterator.class.isInstance(structIterator)) {
+      return ((CollectionAggregateIterator) structIterator).getStatistics();
+
+    } else if (structIterator instanceof MovableLengthsIterator) {
+      MovableLengthsIterator iterator = (MovableLengthsIterator) structIterator;
+      CollectionStatistics stats = new CollectionStatistics();
+      stats.fieldName = root.toString();
+      stats.minLength = Integer.MAX_VALUE;
+
+      while (!iterator.isDone()) {
+        sc.document = iterator.currentCandidate();
+        if (iterator.hasMatch(iterator.currentCandidate())) {
+          int len = iterator.getCurrentLength();
+          stats.collectionLength += len;
+          stats.documentCount += 1;
+          stats.nonZeroLenDocCount += (len > 0) ? 1 : 0;
+          stats.maxLength = Math.max(stats.maxLength, len);
+          stats.minLength = Math.min(stats.minLength, len);
+        }
+        iterator.movePast(sc.document);
+      }
+
+      stats.avgLength = (stats.documentCount > 0) ? (double) stats.collectionLength / (double) stats.documentCount : 0;
+      stats.minLength = (stats.documentCount > 0) ? stats.minLength : 0;
+      return stats;
+    }
+    throw new IllegalArgumentException("Node " + root.toString() + " is not a lengths iterator.");
+  }
+
+  @Override
+  public NodeStatistics getNodeStatistics(String nodeString) throws Exception {
+    // first parse the node
+    Node root = StructuredQuery.parse(nodeString);
+    NodeStatistics ns = getNodeStatistics(root);
+    return ns;
+  }
+
+  @Override
+  public NodeStatistics getNodeStatistics(Node root) throws Exception {
+    ScoringContext sc = ContextFactory.createContext(globalParameters);
+    MovableIterator structIterator = createIterator(new Parameters(), root, sc);
+    if (NodeAggregateIterator.class.isInstance(structIterator)) {
+      return ((NodeAggregateIterator) structIterator).getStatistics();
+
+    } else if (structIterator instanceof MovableCountIterator) {
+
+      NodeStatistics stats = new NodeStatistics();
+      // set up initial values
+      stats.node = root.toString();
+      stats.nodeDocumentCount = 0;
+      stats.nodeFrequency = 0;
+      stats.maximumCount = 0;
+
+      MovableCountIterator iterator = (MovableCountIterator) structIterator;
+
+      while (!iterator.isDone()) {
+        sc.document = iterator.currentCandidate();
+        if (iterator.hasMatch(iterator.currentCandidate())) {
+          stats.nodeFrequency += iterator.count();
+          stats.maximumCount = Math.max(iterator.count(), stats.maximumCount);
+          stats.nodeDocumentCount++;
+        }
+        iterator.movePast(iterator.currentCandidate());
+      }
+
+      return stats;
+    }
+    // otherwise :
+    throw new IllegalArgumentException("Node " + root.toString() + " is not a count iterator.");
+  }
+
+  @Override
+  public NodeType getNodeType(Node node) throws Exception {
+    NodeType nodeType = index.getNodeType(node);
+    if (nodeType == null) {
+      nodeType = features.getNodeType(node);
+    }
+    return nodeType;
+  }
+
+  @Override
+  public QueryType getQueryType(Node node) throws Exception {
+    if (node.getOperator().equals("text")) {
+      return QueryType.UNKNOWN;
+    }
+    NodeType nodeType = getNodeType(node);
+    Class outputClass = nodeType.getIteratorClass();
+    if (ScoreIterator.class.isAssignableFrom(outputClass)
+            || ScoringFunctionIterator.class.isAssignableFrom(outputClass)) {
+      return QueryType.RANKED;
+    } else if (IndicatorIterator.class.isAssignableFrom(outputClass)) {
+      return QueryType.BOOLEAN;
+    } else if (CountIterator.class.isAssignableFrom(outputClass)) {
+      return QueryType.COUNT;
+//    } else if (LengthsIterator.class.isAssignableFrom(outputClass)) {
+//      return QueryType.LENGTH;
+    } else {
+      return QueryType.RANKED;
+    }
+  }
+
+  @Override
+  public Integer getDocumentLength(Integer docid) throws IOException {
+    return index.getLength(docid);
+  }
+
+  @Override
+  public Integer getDocumentLength(String docname) throws IOException {
+    return index.getLength(index.getIdentifier(docname));
+  }
+
+  @Override
+  public String getDocumentName(Integer docid) throws IOException {
+    return index.getName(docid);
+  }
+
+  public Integer getDocumentId(String docname) throws IOException {
+    return index.getIdentifier(docname);
+  }
+
+  public List<Integer> getDocumentIds(List<String> docnames) throws IOException {
+    ArrayList<Integer> internalDocBuffer = new ArrayList<Integer>();
+
+    for (String name : docnames) {
+      try {
+        internalDocBuffer.add(index.getIdentifier(name));
+      } catch (Exception e) {
+        // ignore missing document-names (they could be from other index shards)
+      }
+    }
+    return internalDocBuffer;
+  }
+
+  @Override
+  public void addNodeToCache(Node node) throws Exception {
+    if (cache != null) {
+      cache.addToCache(node, this.createIterator(new Parameters(), node, new ScoringContext()));
+    }
+  }
+
+  @Override
+  public void addAllNodesToCache(Node node) throws Exception {
+    if (cache != null) {
+      // recursivly add all nodes
+      for (Node child : node.getInternalNodes()) {
+        addAllNodesToCache(child);
+      }
+
+      cache.addToCache(node, this.createIterator(new Parameters(), node, new ScoringContext()));
+    }
+  }
+}
---
+++ core/src/main/java/org/lemurproject/galago/core/retrieval/iterator/PassageFilterIterator.java
@@ -0,0 +1,120 @@
+// BSD License (http://lemurproject.org/galago-license)
+package org.lemurproject.galago.core.retrieval.iterator;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+import org.lemurproject.galago.core.retrieval.query.NodeParameters;
+import org.lemurproject.galago.core.retrieval.processing.PassageScoringContext;
+import org.lemurproject.galago.core.retrieval.processing.ScoringContext;
+import org.lemurproject.galago.core.retrieval.query.AnnotatedNode;
+import org.lemurproject.galago.core.util.ExtentArray;
+
+/**
+ *
+ * @author irmarc
+ */
+public class PassageFilterIterator extends TransformIterator implements MovableExtentIterator, MovableCountIterator {
+
+  MovableExtentIterator extentIterator;
+  PassageScoringContext passageContext;
+  int begin, end, docid;
+  ExtentArray cached;
+  protected byte[] key;
+
+  public PassageFilterIterator(NodeParameters parameters, MovableExtentIterator extentIterator) {
+    super(extentIterator);
+    this.extentIterator = extentIterator;
+    this.cached = new ExtentArray();
+    docid = -1;
+    key = extentIterator.key();
+  }
+
+  @Override
+  public byte[] key() {
+    return key;
+  }
+
+  @Override
+  public boolean hasMatch(int document){
+    return extents().size() > 0;
+  }
+  
+  /**
+   * Filters out extents that are not in the range of the current passage
+   * window.
+   *
+   * @return
+   */
+  @Override
+  public ExtentArray extents() {
+    if (passageContext == null) {
+      return extentIterator.extents();
+    }
+
+    if (docid != passageContext.document || begin != passageContext.begin
+            || end != passageContext.end) {
+      loadExtents();
+    }
+    return cached;
+  }
+
+  private void loadExtents() {
+    cached.reset();
+    ExtentArray internal = extentIterator.extents();
+
+    if (passageContext != null) {
+      for (int i = 0; i < internal.size(); i++) {
+        if (internal.begin(i) >= passageContext.begin
+                && internal.end(i) <= passageContext.end) {
+          cached.add(internal.begin(i), internal.end(i));
+        }
+      }
+      docid = passageContext.document;
+      begin = passageContext.begin;
+      end = passageContext.end;
+    }
+  }
+
+  @Override
+  public void setContext(ScoringContext context) {
+    super.setContext(context);
+
+    if (!PassageScoringContext.class.isAssignableFrom(context.getClass())) {
+      // debugging info line.
+      // Logger.getLogger(PassageFilterIterator.class.getName()).info("Setting a non-Passage-capable context as a PassageScoringContext - passages can not be used.");
+      passageContext = null;
+    } else {
+      passageContext = (PassageScoringContext) context;
+    }
+  }
+
+  @Override
+  public ExtentArray getData() {
+    return extents();
+  }
+
+  @Override
+  public int count() {
+    return extents().size();
+  }
+
+  @Override
+  public int maximumCount() {
+    return this.extentIterator.maximumCount();
+  }
+
+  @Override
+  public AnnotatedNode getAnnotatedNode() throws IOException {
+    String type = "extent";
+    String className = this.getClass().getSimpleName();
+    String parameters = "";
+    int document = currentCandidate();
+    boolean atCandidate = hasMatch(this.context.document);
+    String returnValue = extents().toString();
+    List<AnnotatedNode> children = Collections.singletonList(extentIterator.getAnnotatedNode());
+
+    return new AnnotatedNode(type, className, parameters, document, atCandidate, returnValue, children);
+  }
+}
