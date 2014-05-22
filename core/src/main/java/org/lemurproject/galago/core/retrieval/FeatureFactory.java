5ef700b7188ef8b6ecd31f40cca6631862736e11
John Foley <jfoley@cs.umass.edu>
2014-05-22 17:29:18 -0400

    Add close() to DiskMapReader and function for dumping lengths without names

---
+++ core/src/main/java/org/lemurproject/galago/core/retrieval/FeatureFactory.java
@@ -0,0 +1,366 @@
+// BSD License (http://lemurproject.org/galago-license)
+package org.lemurproject.galago.core.retrieval;
+
+import org.lemurproject.galago.core.retrieval.iterator.*;
+import org.lemurproject.galago.core.retrieval.iterator.scoring.*;
+import org.lemurproject.galago.core.retrieval.query.Node;
+import org.lemurproject.galago.core.retrieval.query.NodeParameters;
+import org.lemurproject.galago.core.retrieval.query.NodeType;
+import org.lemurproject.galago.core.retrieval.traversal.*;
+import org.lemurproject.galago.core.retrieval.traversal.optimize.ExtentsToCountLeafTraversal;
+import org.lemurproject.galago.core.retrieval.traversal.optimize.FlattenCombineTraversal;
+import org.lemurproject.galago.core.retrieval.traversal.optimize.FlattenWindowTraversal;
+import org.lemurproject.galago.core.retrieval.traversal.optimize.MergeCombineChildrenTraversal;
+import org.lemurproject.galago.tupleflow.Parameters;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+/**
+ * @author trevor
+ * @author irmarc
+ * @author sjh
+ */
+public class FeatureFactory {
+
+  protected HashMap<String, OperatorSpec> operatorLookup;
+  protected List<TraversalSpec> traversals;
+  protected Parameters parameters;
+  static String[][] sOperatorLookup = {
+    {ThresholdIterator.class.getName(), "threshold"},
+    {ScoreCombinationIterator.class.getName(), "combine"},
+    {WeightedSumIterator.class.getName(), "wsum"},
+    {SynonymIterator.class.getName(), "syn"},
+    {SynonymIterator.class.getName(), "synonym"},
+    {ExtentInsideIterator.class.getName(), "inside"},
+    {MinCountIterator.class.getName(), "mincount"},
+    {OrderedWindowIterator.class.getName(), "ordered"},
+    {OrderedWindowIterator.class.getName(), "od"},
+    {BigramIterator.class.getName(), "bigram"},
+    {OrderedWindowIterator.class.getName(), "quote"}, // don't rely on this - ImplicitFeatureCast does: quote -> od:1 for now. (irmarc)
+    {UnorderedWindowIterator.class.getName(), "unordered"},
+    {UnorderedWindowIterator.class.getName(), "uw"},
+    {UnorderedWindowBigramIterator.class.getName(), "ubigram"},
+    {UnorderedWindowIterator.class.getName(), "intersect"},
+    {ScaleIterator.class.getName(), "scale"},
+    {ScoreCombinationIterator.class.getName(), "rm"},
+    {ScoreCombinationIterator.class.getName(), "bm25rf"},
+    {BM25FCombinationIterator.class.getName(), "bm25fcomb"},
+    {BM25FCombinationIterator.class.getName(), "bm25fcomb"},
+    {UniversalIndicatorIterator.class.getName(), "all"},
+    {ExistentialIndicatorIterator.class.getName(), "any"},
+    {NullExtentIterator.class.getName(), "null"},
+    {RequireIterator.class.getName(), "require"},
+    {RejectIterator.class.getName(), "reject"},
+    {GreaterThanIterator.class.getName(), "greater"},
+    {LessThanIterator.class.getName(), "less"},
+    {InBetweenIterator.class.getName(), "between"},
+    {EqualityIterator.class.getName(), "equals"},
+    {PassageFilterIterator.class.getName(), "passagefilter"},
+    {PL2ScoringIterator.class.getName(), "pl2scorer"},
+    {PassageLengthIterator.class.getName(), "passagelengths"},
+    {LogProbNotIterator.class.getName(), "logprobnot"},
+    // Scorers can be named directly as nodes
+    {DirichletScoringIterator.class.getName(), "dirichlet"},
+    {JelinekMercerScoringIterator.class.getName(), "linear"},
+    {JelinekMercerScoringIterator.class.getName(), "jm"},
+    {BM25ScoringIterator.class.getName(), "bm25"},
+    {BM25RFScoringIterator.class.getName(), "bm25rf"},
+    {BoostingIterator.class.getName(), "boost"},
+    {LogarithmIterator.class.getName(), "log"},
+    {PL2ScoringIterator.class.getName(), "pl2"},
+    {InL2ScoringIterator.class.getName(), "inl2"},
+    {BiL2ScoringIterator.class.getName(), "bil2"}
+  };
+  static String[] sTraversalList = {
+    RelevanceModelTraversal.class.getName(),
+    ReplaceOperatorTraversal.class.getName(),
+    StopStructureTraversal.class.getName(),
+    StopWordTraversal.class.getName(),
+    WeightedSequentialDependenceTraversal.class.getName(),
+    WeightedSequentialDependenceTraversal.class.getName(),
+    SequentialDependenceTraversal.class.getName(),
+    FullDependenceTraversal.class.getName(),
+    ProximityDFRTraversal.class.getName(),
+    PRMS2Traversal.class.getName(),
+    TransformRootTraversal.class.getName(),
+    WindowRewriteTraversal.class.getName(),
+    TextFieldRewriteTraversal.class.getName(),
+    PartAssignerTraversal.class.getName(),
+    InsideToFieldPartTraversal.class.getName(),
+    ImplicitFeatureCastTraversal.class.getName(),
+    InsertLengthsTraversal.class.getName(),
+    PassageRestrictionTraversal.class.getName(),
+    ExtentsToCountLeafTraversal.class.getName(),
+    FlattenWindowTraversal.class.getName(),
+    FlattenCombineTraversal.class.getName(),
+    MergeCombineChildrenTraversal.class.getName(),
+    AnnotateParameters.class.getName(),
+    AnnotateCollectionStatistics.class.getName(),
+    DeltaCheckTraversal.class.getName()
+  };
+
+  public FeatureFactory(Parameters p) {
+    this(p, sOperatorLookup, sTraversalList);
+  }
+
+  public FeatureFactory(Parameters parameters,
+          String[][] sOperatorLookup,
+          String[] sTraversalList) {
+    operatorLookup = new HashMap<String, OperatorSpec>();
+    this.parameters = parameters;
+
+    for (String[] item : sOperatorLookup) {
+      OperatorSpec operator = new OperatorSpec();
+      operator.className = item[0];
+      String operatorName = item[1];
+      operatorLookup.put(operatorName, operator);
+    }
+
+    ArrayList<TraversalSpec> afterTraversals = new ArrayList<TraversalSpec>();
+    ArrayList<TraversalSpec> beforeTraversals = new ArrayList<TraversalSpec>();
+    ArrayList<TraversalSpec> insteadTraversals = new ArrayList<TraversalSpec>();
+
+    if (parameters.isMap("traversals") || parameters.isList("traversals", Parameters.class)) {
+      List<Parameters> traversals = parameters.getAsList("traversals", Parameters.class);
+      for (Parameters traversal : traversals) {
+        String className = traversal.getString("name");
+        String order = traversal.get("order", "after");
+
+        TraversalSpec spec = new TraversalSpec();
+        spec.className = className;
+
+        if (order.equals("before")) {
+          beforeTraversals.add(spec);
+        } else if (order.equals("after")) {
+          afterTraversals.add(spec);
+        } else if (order.equals("instead")) {
+          insteadTraversals.add(spec);
+        } else {
+          throw new RuntimeException("order must be one of {before,after,instead}");
+        }
+      }
+    }
+
+    // If the user doesn't want to replace the current pipeline, add in the std pipeline
+    if (insteadTraversals.size() == 0) {
+      for (String className : sTraversalList) {
+        TraversalSpec spec = new TraversalSpec();
+        spec.className = className;
+        insteadTraversals.add(spec);
+      }
+    }
+
+
+    traversals = new ArrayList<TraversalSpec>();
+    traversals.addAll(beforeTraversals);
+    traversals.addAll(insteadTraversals);
+    traversals.addAll(afterTraversals);
+
+    // Load external operators
+    if (parameters.containsKey("operators")) {
+      Parameters operators = parameters.getMap("operators");
+      for (String operatorName : operators.getKeys()) {
+        String className = operators.getString(operatorName);
+        OperatorSpec spec = new OperatorSpec();
+        spec.className = className;
+        operatorLookup.put(operatorName, spec);
+      }
+    }
+  }
+
+  public static class OperatorSpec {
+
+    public String className;
+  }
+
+  public static class TraversalSpec {
+
+    public String className;
+  }
+
+  public String getClassName(Node node) throws Exception {
+    String operator = node.getOperator();
+
+    OperatorSpec operatorType = operatorLookup.get(operator);
+
+    if (operatorType == null) {
+      return null;
+    }
+
+    // This is to compensate for the transparent behavior of the fitering nodes
+    return operatorType.className;
+  }
+
+  @SuppressWarnings("unchecked")
+  public Class<BaseIterator> getClass(Node node) throws Exception {
+    String className = getClassName(node);
+    if (className == null) {
+      return null;
+    }
+    Class c = Class.forName(className);
+
+    if (BaseIterator.class.isAssignableFrom(c)) {
+      return (Class<BaseIterator>) c;
+    } else {
+      return null;
+    }
+  }
+
+  public NodeType getNodeType(Node node) throws Exception {
+    Class<BaseIterator> cls = getClass(node);
+    if (cls != null) {
+      return new NodeType(getClass(node));
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Given a query node, generates the corresponding iterator object that can be
+   * used for structured retrieval. This method just calls getClass() on the
+   * node, then instantiates the resulting class.
+   *
+   * If the class returned by getClass() is a ScoringFunction, it must contain a
+   * constructor that takes a single Parameters object. If the class returned by
+   * getFeatureClass() is some kind of Iterator, it must take a
+   * Parameters object and an ArrayList of DocumentDataIterators as parameters.
+   */
+  public BaseIterator getIterator(Node node, ArrayList<BaseIterator> childIterators) throws Exception {
+    // One type of constructor allowed: Parameters?, NodeParameters?, child+
+    // Anything not conforming to that gets an exception
+
+    // Get the matching class for the node
+    Class<? extends BaseIterator> c = getClass(node);
+    if (c == null) {
+      throw new IllegalArgumentException("Couldn't get the class of node: "+node);
+    }
+
+    // There better be only 1 constructor
+    Constructor[] cons = c.getConstructors();
+    Constructor constructor;
+    ArrayList<Object> arguments = new ArrayList<Object>();
+    LinkedList<Class> formals = new LinkedList<Class>();
+    boolean fail = false;
+    String failStr = "<unknown>";
+    int ic = 0;
+    for (; ic < cons.length; ic++) {
+      fail = false;
+      constructor = cons[ic];
+      arguments.clear();
+      formals.clear();
+
+      // Construct our argument list as we zip down the list of formal parameters
+      formals.addAll(Arrays.asList(constructor.getParameterTypes()));
+      int childIdx = 0;
+      while (formals.size() > 0) {
+        if (formals.get(0) == NodeParameters.class) {
+          // Only valid if at the front or preceded by immutables
+          if (arguments.isEmpty()) {
+            NodeParameters params = node.getNodeParameters().clone();
+            arguments.add(params);
+          } else {
+            fail = true;
+            failStr = "NodeParameters must be the first argument, not the " + arguments.size() + "-th.";
+            break;
+          }
+        } else if (BaseIterator.class.isAssignableFrom(formals.get(0))) {
+          // Some number of Iterator, can be different - just do the one at the front now
+          if (formals.get(0).isAssignableFrom(childIterators.get(childIdx).getClass())) {
+            arguments.add(childIterators.get(childIdx));
+            childIdx++;
+          } else {
+            fail = true;
+            failStr = "Argument " + arguments.size() + " is:\n" + childIterators.get(childIdx).getClass().getName() + "\nConstructor expected:\n" + formals.get(0).getName();
+            break;
+          }
+        } else if (formals.get(0).isArray()) {
+          // Only an array of structured iterators - all the same type
+          // First check that all children match
+          Class ac = formals.get(0).getComponentType();
+          for (int i = childIdx; i < childIterators.size(); i++) {
+            if (!ac.isAssignableFrom(childIterators.get(i).getClass())) {
+              fail = true;
+              failStr = "Argument " + arguments.size() + " is:\n" + childIterators.get(i).getClass().getName() + "\nConstructor expected an array of:\n" + ac.getName();
+              break;
+            }
+          }
+
+          if (fail) {
+            break;
+          }
+          Object typedArray = Array.newInstance(formals.get(0).getComponentType(), 0);
+          Object[] generalArray = childIterators.subList(childIdx, childIterators.size()).toArray((Object[]) typedArray);
+          arguments.add(generalArray);
+        }
+        formals.poll();
+      }
+
+      if (!fail) {
+        break;
+      }
+    }
+
+    if (fail) {
+      StringBuilder msg = new StringBuilder();
+      msg.append(String.format("No valid constructor for node %s.\n", node.toString()));
+      msg.append("Allowable Iterator constructors allow for leading optional Parameters,");
+      msg.append(" followed by optional NodeParameters, and finally the list of child iterators.");
+      msg.append("FAILED AT: ").append(failStr);
+      throw new IllegalArgumentException(msg.toString());
+    }
+
+    return (BaseIterator) cons[ic].newInstance(arguments.toArray(new Object[0]));
+  }
+
+  public List<String> getTraversalNames() {
+    ArrayList<String> result = new ArrayList<String>();
+    for (TraversalSpec spec : traversals) {
+      result.add(spec.className);
+    }
+    return result;
+  }
+
+  public List<Traversal> getTraversals(Retrieval retrieval)
+          throws ClassNotFoundException, NoSuchMethodException, InstantiationException,
+          IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    ArrayList<Traversal> result = new ArrayList<Traversal>();
+    
+    for (TraversalSpec spec : traversals) {
+      Class<? extends Traversal> traversalClass =
+              (Class<? extends Traversal>) Class.forName(spec.className);
+      Constructor[] constructors = traversalClass.getConstructors();
+      Traversal traversal = null;
+      
+      // try to construct a traversal with a retrieval
+      for (Constructor c : constructors) {
+        Class[] argTypes = c.getParameterTypes();
+        if (argTypes.length == 1 && argTypes[0].isAssignableFrom(Retrieval.class)) {
+          traversal = (Traversal) c.newInstance(retrieval);
+          break;
+        }
+      }
+      
+      // allow a traversal with no retrieval
+      if(traversal == null) {
+        for(Constructor c : constructors) {
+          Class[] argTypes = c.getParameterTypes();
+          if (argTypes.length == 0) {
+            traversal = (Traversal) c.newInstance();
+            break;
+          }
+        }
+      }
+      
+      if(traversal == null) {
+        throw new IllegalArgumentException("Traversals should have obvious constructors, failed on: " + traversalClass);
+      }
+
+      result.add(traversal);
+    }
+
+    return result;
+  }
+}
---
+++ core/src/main/java/org/lemurproject/galago/core/tools/apps/BatchSearch.java
@@ -0,0 +1,176 @@
+// BSD License (http://lemurproject.org/galago-license)
+package org.lemurproject.galago.core.tools.apps;
+
+import org.lemurproject.galago.core.retrieval.Retrieval;
+import org.lemurproject.galago.core.retrieval.RetrievalFactory;
+import org.lemurproject.galago.core.retrieval.ScoredDocument;
+import org.lemurproject.galago.core.retrieval.query.Node;
+import org.lemurproject.galago.core.retrieval.query.StructuredQuery;
+import org.lemurproject.galago.core.tools.AppFunction;
+import org.lemurproject.galago.tupleflow.Parameters;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author sjh
+ */
+public class BatchSearch extends AppFunction {
+
+  public static final Logger logger = Logger.getLogger("BatchSearch");
+
+  public static void main(String[] args) throws Exception {
+    (new BatchSearch()).run(Parameters.parseArgs(args), System.out);
+  }
+
+  @Override
+  public String getName() {
+    return "batch-search";
+  }
+
+  @Override
+  public String getHelpString() {
+    return "galago simple-batch-search <args>\n\n"
+            + "  Runs a batch of queries against an index and produces TREC-formatted\n"
+            + "  output.  The output can be used with retrieval evaluation tools like\n"
+            + "  galago eval (org.lemurproject.galago.core.eval).\n\n"
+            + "  Sample invocation:\n"
+            + "     galago batch-search --index=/tmp/myindex --requested=200 /tmp/queries.json \n\n"
+            + "  Args:\n"
+            + "     --index=path_to_your_index\n"
+            + "     --requested : Number of results to return for each query, default=1000\n"
+            + "     /path/to/query/file.json : Input file in xml parameters format (see below).\n\n"
+            + "  Query file format:\n"
+            + "    The query file is an JSON file containing a set of queries.  Each query\n"
+            + "    has text field, which contains the text of the query, and a number field, \n"
+            + "    which uniquely identifies the query in the output.\n\n"
+            + "  Example query file:\n"
+            + "  {\n"
+            + "     \"queries\" : [\n"
+            + "       {\n"
+            + "         \"number\" : \"CACM-408\", \n"
+            + "         \"text\" : \"#combine(my query)\"\n"
+            + "       },\n"
+            + "       {\n"
+            + "         \"number\" : \"WIKI-410\", \n"
+            + "         \"text\" : \"#combine(another query)\" \n"
+            + "       }\n"
+            + "    ]\n"
+            + "  }\n";
+  }
+
+  @Override
+  public void run(Parameters parameters, PrintStream out) throws Exception {
+    List<ScoredDocument> results = null;
+
+    if (!(parameters.containsKey("query")
+            || parameters.containsKey("queries"))) {
+      out.println(this.getHelpString());
+      return;
+    }
+
+    // ensure we can print to a file instead of the commandline
+    if (parameters.isString("outputFile")) {
+      boolean append = parameters.get("appendFile", false);
+      out = new PrintStream(new BufferedOutputStream(
+              new FileOutputStream(parameters.getString("outputFile"), append)));
+    }
+
+    // get queries
+    List<Parameters> queries = collectQueries(parameters);
+
+    // open index
+    Retrieval retrieval = RetrievalFactory.instance(parameters);
+
+    // record results requested
+    int requested = (int) parameters.get("requested", 1000);
+
+    // for each query, run it, get the results, print in TREC format
+
+    for (Parameters query : queries) {
+      String queryText = query.getString("text");
+      String queryNumber = query.getString("number");
+
+
+      query.setBackoff(parameters);
+      query.set("requested", requested);
+
+      // option to fold query cases -- note that some parameters may require upper case
+      if (query.get("casefold", false)) {
+        queryText = queryText.toLowerCase();
+      }
+
+      if (parameters.get("verbose", false)) {
+        logger.info("RUNNING: " + queryNumber + " : " + queryText);
+      }
+
+      // parse and transform query into runnable form
+      Node root = StructuredQuery.parse(queryText);
+      Node transformed = retrieval.transformQuery(root, query);
+
+      if (parameters.get("verbose", false)) {
+        logger.info("Transformed Query:\n" + transformed.toPrettyString());
+      }
+
+      // run query
+      results = retrieval.executeQuery(transformed, query).scoredDocuments;
+      
+
+      // if we have some results -- print in to output stream
+      if (!results.isEmpty()) {
+        for (ScoredDocument sd : results) {
+          if (query.get("trec", false)) {
+            out.println(sd.toTRECformat(queryNumber));
+          } else {
+            out.println(sd.toString(queryNumber));
+          }
+        }
+      }
+    }
+
+    if (parameters.isString("outputFile")) {
+      out.close();
+    }
+  }
+
+  /**
+   * this function extracts a list of queries from a parameter object. - there
+   * are several methods of inputting queries: (query/queries) ->
+   * String/List(String)/List(Map)
+   *
+   * if List(Map): [{"number":"id", "text":"query text"}, ...]
+   */
+  public static List<Parameters> collectQueries(Parameters parameters) throws IOException {
+    List<Parameters> queries = new ArrayList();
+    int unnumbered = 0;
+    if (parameters.isString("query") || parameters.isList("query", String.class)) {
+      String id;
+      for (String q : (List<String>) parameters.getAsList("query")) {
+        id = "unk-" + unnumbered;
+        unnumbered++;
+        queries.add(Parameters.parseString(String.format("{\"number\":\"%s\", \"text\":\"%s\"}", id, q)));
+      }
+    }
+    if (parameters.isString("queries") || parameters.isList("queries", String.class)) {
+      String id;
+      for (String q : (List<String>) parameters.getAsList("query")) {
+        id = "unk-" + unnumbered;
+        unnumbered++;
+        queries.add(Parameters.parseString(String.format("{\"number\":\"%s\", \"text\":\"%s\"}", id, q)));
+      }
+    }
+    if (parameters.isList("query", Parameters.class)) {
+      queries.addAll(parameters.getList("query"));
+    }
+    if (parameters.isList("queries", Parameters.class)) {
+      queries.addAll(parameters.getList("queries"));
+    }
+    return queries;
+  }
+}
